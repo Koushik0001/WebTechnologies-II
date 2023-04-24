ffc894975a10dadee463d73bf10469b8e01223a9
Koushik Mahanta <mahantakoushik80@gmail.com>
2023-04-25 02:26:47 +0530

    Upgraded the chat application

--- WEB-INF/sources/websocket/ChatEndpoint.java
+++ WEB-INF/sources/websocket/ChatEndpoint.java
@@ -4,6 +4,8 @@ import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime;    
 
 import javax.websocket.CloseReason;
 import javax.websocket.Endpoint;
@@ -19,23 +21,29 @@ import javax.websocket.Session;
 public class ChatEndpoint {
 
     static List<ChatEndpoint> clients = new CopyOnWriteArrayList<ChatEndpoint>(); 
+    static int noOfClients = 0;
     Session session;
+    String name;
 
     @OnOpen
     public void onOpen(Session session, EndpointConfig endpointConfig){
         this.session = session;
+        this.name = "User"+noOfClients;
         clients.add(this);
+        noOfClients++;
+        broadcast("\n" +this.name + " Joined\nTime: " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now())+"\n");
     }
 
     @OnClose
     public void OnClose(Session session, CloseReason reason){
         System.out.println("Socked Closed: "+ reason.getReasonPhrase());
         clients.remove(this);
+        broadcast("\n"+this.name + " left\nTime: " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now())+"\n");
     }
 
     @OnMessage
     public void onMessage(String message){
-        broadcast(message);
+        broadcast(this.name + " : " + message);
     }
 
     private void broadcast(String message){
